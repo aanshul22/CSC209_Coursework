73d97e27eb624d223ed98049686b13c7e1793abb
Anshul <anshul.agrawal@mail.utoronto.ca>
2021-01-28 03:29:48 +0530

    fixed fibonacci

--- lab3/fibonacci.c
+++ lab3/fibonacci.c
@@ -18,12 +18,17 @@
 void fib(int ** seq, int n) {
     *seq = malloc(sizeof(int) * n);
     int * l = *seq;
-    l[0] = 0;
-    l[1] = 1;
 
-    for(int i = 2; i < n; i++) {
-        l[i] = l[i - 1] + l[i - 2];
+    l[0] = 0;
+
+    if (n > 1) {
+        l[1] = 1;
+
+        for(int i = 2; i < n; i++) {
+            l[i] = l[i - 1] + l[i - 2];
+        }
     }
+
 }
 
 int main(int argc, char **argv) {
