69aa9135dcd7ec59218c29960dc4fe2012e8f7d4
Anshul <anshul.agrawal@mail.utoronto.ca>
2021-03-24 01:40:22 +0530

    Removed extra files

--- a3/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-./classifier -p 48 -K 1 -d euc a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 2 -d euc a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 3 -d euc a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 4 -d euc a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 1 -d cos a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 2 -d cos a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 3 -d cos a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
-./classifier -p 48 -K 4 -d cos a2_datasets/training_data.bin a2_datasets/testing_data.bin & >> results.txt
